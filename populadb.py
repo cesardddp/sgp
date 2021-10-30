0b737455dc9f7ecd5749b0abfb5b17ab5bc6e2a8
cesardddp <cesardddp@hotmail.com>
2021-10-29 21:41:39 -0300

    add popula.py

---
+++ populadb.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from requests import get, post
+from ipdb import set_trace
+from multiprocessing import Process
+
+url = "http://localhost:5050/cliente/novo"
+url = "https://sgpdiego.pythonanywhere.com/cliente/novo"
+df = pd.read_csv("sgp_real.csv")
+
+
+def cria_cliente(row):
+    try:
+        cliente = {
+            "nome": str(row["CLIENTE"]) or "",
+            "endereco": str(row["ENDEREÇO"]) or "",
+            "telefone": str(row["TELEFONE"]) or "",
+        }
+        print(cliente)
+        print(post(url, json=cliente))
+    except Exception as erro:
+        print(erro)
+        set_trace()
+
+
+from .schema import ClienteSchema, Cliente,Usuario
+from . import db
+
+schema = ClienteSchema()
+
+
+def popula_db():
+    global df
+    # import ipdb;ipdb.set_trace()
+    if Usuario.query.filter_by(nome="cesar").first():return
+    for index, row in df.iterrows():
+        # Process(cria_cliente(row)).start()
+        try:
+            cliente = {
+                "nome": str(row["CLIENTE"]) or "",
+                "endereco": str(row["ENDEREÇO"]) or "",
+                "telefone": str(row["TELEFONE"]) or "",
+            }
+            db.session.add(
+                Cliente(**schema.dump(cliente))
+            )
+            print(cliente)
+        except Exception as erro:
+            print(erro)
+            set_trace()
+    
+    db.session.add(
+        Usuario(
+            "cesar","teste"
+        )
+    )
+    db.session.commit()
+    # import ipdb;ipdb.set_trace()
